f18bc2a372a3d7a6dc49dd86fe4976eb687d3116
littleliang <littleliang@tencent.com>
2018-05-22 18:06:04 +0800

    first version

--- PB2JSON/main.cpp
+++ PB2JSON/main.cpp
@@ -14,29 +14,29 @@
 int main(int argc, char *argv[]) {
 //    ProtobufMsg message;
     
-    argc = 5;
-
-    char **argv1 = new char *[3];
-    argv1[0] = new char[200];
-    strcpy(argv1[0], argv[0]);
-
-    argv1[1] = new char[200];
-    strcpy(argv1[1], "./BaseResponse.proto");
+//    argc = 5;
 //
-    argv1[2] = new char[200];
-    strcpy(argv1[2], "./TargetResponse.proto");
+//    char **argv1 = new char *[3];
+//    argv1[0] = new char[200];
+//    strcpy(argv1[0], argv[0]);
 //
-    
-    argv1[3] = new char[200];
-    strcpy(argv1[3], "--target=CgiF2FPaySucPageResp");
-    
-    argv1[4] = new char[200];
-    strcpy(argv1[4], "--mockcase_out=./");
-    
+//    argv1[1] = new char[200];
+//    strcpy(argv1[1], "./BaseResponse.proto");
+////
+//    argv1[2] = new char[200];
+//    strcpy(argv1[2], "./TargetResponse.proto");
+////
+//
+//    argv1[3] = new char[200];
+//    strcpy(argv1[3], "--target=CgiF2FPaySucPageResp");
+//
+//    argv1[4] = new char[200];
+//    strcpy(argv1[4], "--mockcase_out=./");
+//
     google::protobuf::compiler::CommandLineInterface cli;
     
     google::protobuf::compiler::objectivec::ObjectiveCGenerator objc_generator;
     cli.RegisterGenerator("--mockcase_out", &objc_generator, "Generate Mock Case Script File");
     
-    return cli.Run(argc, argv1);
+    return cli.Run(argc, argv);
 }
